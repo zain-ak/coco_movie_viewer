76d2ef0bb00e02fc400786bf1a414b9fce76eff3
Zain Khalid <zak565@uowmail.edu.au>
2019-10-29 19:48:31 -0400

    Adding Classes and structure

---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+const String apiKey = 'eecdd7e4d2f0d8a98f7b97e561b0eb1c';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
