ba0a00e12190720d57e68fb59c5e7c9f00872b54
Zain Khalid <zak565@uowmail.edu.au>
2019-11-05 21:14:29 -0500

    Delete constants.dart

--- lib/constants.dart
+++
@@ -1,7 +0,0 @@
-class Constant {
-    static const String apiKey = 'eecdd7e4d2f0d8a98f7b97e561b0eb1c';
-    static const String baseURL = 'https://api.themoviedb.org/3/';
-    static const String ImageBaseURL = 'https://image.tmdb.org/t/p/w500';
-    static const String ytApiKey = 'AIzaSyAB994tzlPQ_jeqI-ilw6vk8tm5JB1XRl8';
-
-}
